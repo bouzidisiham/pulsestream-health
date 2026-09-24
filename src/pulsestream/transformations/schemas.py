2b5a7c2a9c616e8e47049d02febb2ba278e46f20
Siham Bouzidi <bouzidisiham1997@gmail.com>
2026-09-24 23:31:01 +0200

    feat(transformations): add DDL schemas for patients and vitals

---
+++ src/pulsestream/transformations/schemas.py
@@ -0,0 +1,2 @@
+PATIENTS_SCHEMA = "id_patient STRING NOT NULL, prenom STRING, nom STRING, date_naissance DATE, sexe STRING, groupe_sanguin STRING, ville STRING, taille_cm INT, poids_kg DOUBLE, antecedents STRING, allergies STRING"
+VITALS_SCHEMA = "id_patient STRING NOT NULL, horodatage TIMESTAMP, frequence_cardiaque INT, tension_systolique INT, tension_diastolique INT, temperature DOUBLE, saturation_oxygene INT"
